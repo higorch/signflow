47396eb984c9063852a37fc98263289b12a364e8
Higor <higorchristianfe@gmail.com>
2026-07-16 11:31:44 -0300

    route

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('panel.dashboard.index');
+});
+
 Route::group(['prefix' => 'panel', 'as' => 'panel.'], function () {
     Route::livewire('dashboard', 'pages::panel.dashboard.index')->name('dashboard.index');
 
